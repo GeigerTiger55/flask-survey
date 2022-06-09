8d9ad59af8b594151783ee28b3469a5f940bbc45
Joel <jcbluemel@yahoo.com>
2022-06-09 15:19:33 -0700

    review styling

--- app.py
+++ app.py
@@ -15,9 +15,11 @@ responses = []
 def start():
     """Load home page with survey title and instructions"""
 
-    return render_template('survey_start.html',
-                           title=survey.title,
-                           instructions=survey.instructions)
+    return render_template(
+        'survey_start.html',
+        title=survey.title,
+        instructions=survey.instructions,
+    )
 
 
 @app.post("/begin")
@@ -33,9 +35,11 @@ def questions(question_index):
     """Show question page with form containing question and possible answers.
         Passes along current question index"""
 
-    return render_template("question.html",
-                           question=survey.questions[question_index],
-                           question_index=question_index)
+    return render_template(
+        "question.html",
+        question=survey.questions[question_index],
+        question_index=question_index,
+    )
 
 
 @app.post("/answer")
